472499b5dbfd1710aefafcd0d90391eee0f38f21
rmummadu <rmummadu@cisco.com>
2018-12-18 16:22:39 +0530

    added date functionality

--- testproject/src/main/java/common/testproject/Test.java
+++ testproject/src/main/java/common/testproject/Test.java
@@ -3,13 +3,15 @@ package common.testproject;
 public class Test {
 	public static void main(String args[]){  
 		System.out.println("Test Class ");  
-		long start = System.currentTimeMillis();
+/*		long start = System.currentTimeMillis();
 		calc();
 		long end = System.currentTimeMillis();
 		long elapse = end-start;
 		long elapsedTime = (elapse % 3600)/60;
 		System.out.println("Time in sec :"+elapse);
-		System.out.println("Total time taken : "+elapsedTime+" min");
+		System.out.println("Total time taken : "+elapsedTime+" min");*/
+		Test t = new Test();
+		t.stopDecapThread.run();
 	} 
 	
 	public static void calc(){
@@ -19,6 +21,16 @@ public class Test {
 		}
 		System.out.println("count : "+count);
 	}
+	
+	private Runnable stopDecapThread = () -> {
+		try {
+			System.out.println("Stopiing service sleeping.....");
+			Thread.sleep(5000);
+			System.out.println("Stopiing service.....");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	};
 	
 	
 }
